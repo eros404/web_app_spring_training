86032c64efa7508138626acc4f0391b5cf2ce653
Louis Morando <louis.morando@epita.fr>
2022-01-25 15:41:42 +0100

    Database persistence

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -2,16 +2,23 @@ package fr.lernejo.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoListController {
+    private final TodoRepository repository;
+
+    public TodoListController(TodoRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/api/todo")
-    public Todo get() {
-        return new Todo("bonjour !", "Louis");
+    public Iterable<TodoEntity> get() {
+        return repository.findAll();
     }
     @PostMapping("/api/todo")
-    public void post(Todo todo) {
-
+    public void post(@RequestBody TodoEntity todo) {
+        repository.save(todo);
     }
 }
